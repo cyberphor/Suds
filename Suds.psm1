834fec31880edd26db428c7d1cb85d9d7246eb44
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2023-02-12 23:16:50 -0800

    2023-02-12, 2316

---
+++ Suds.psm1
@@ -0,0 +1,15 @@
+. $PSScriptRoot\Suds\ConvertFrom-Base64.ps1
+. $PSScriptRoot\Suds\ConvertTo-Base64.ps1
+. $PSScriptRoot\Suds\ConvertTo-BinaryString.ps1
+. $PSScriptRoot\Suds\ConvertTo-IpAddress.ps1
+. $PSScriptRoot\Suds\Export-Gpo.ps1
+. $PSScriptRoot\Suds\Find-WirelessComputer.ps1
+. $PSScriptRoot\Suds\Get-IpAddressRange.ps1
+. $PSScriptRoot\Suds\Get-ProcessCreationReport.ps1
+. $PSScriptRoot\Suds\Get-SerialNumberAndCurrentUser.ps1
+. $PSScriptRoot\Suds\Install-RSAT.ps1
+. $PSScriptRoot\Suds\New-AdDomainAdmin.ps1
+. $PSScriptRoot\Suds\New-AdForest.ps1
+. $PSScriptRoot\Suds\New-GpoWallpaper.ps1
+. $PSScriptRoot\Suds\Remove-StaleDnsRecord.ps1
+. $PSScriptRoot\Suds\Uninstall-Sysmon.ps1
