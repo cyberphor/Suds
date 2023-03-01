e588bbcfa4790705b76faa9a5a1fb51519f1c118
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2023-02-28 21:45:11 -0800

    Update Start-WinFormDataGridView.ps1

--- Suds/Start-WinFormDataGridView.ps1
+++ Suds/Start-WinFormDataGridView.ps1
@@ -1,14 +1,27 @@
-function Start-WinFormDataGridView {
-  # Data
-  $Processes = Get-Process | Select-Object Name, Id, Description, @{n="Memory"; e={$_.WorkingSet}}
-  $ArrayList = New-Object System.Collections.ArrayList
-  $ArrayList.AddRange($Processes)
-  $Properties = @{Size = New-Object System.Drawing.Size(800,400); ColumnHeadersVisible = $true; DataSource = $ArrayList}
-
-  # Form
+﻿function Start-WinFormDataGridView {
   $Form = New-Object System.Windows.Forms.Form
-  $Form.Size = New-Object System.Drawing.Size(900,600) 
-  $DataGridView = New-Object System.Windows.Forms.DataGridView -Property $Properties
+  $Form.Size = "400,500" 
+  $DataGridView = New-Object System.Windows.Forms.DataGridView
+  $DataGridView.Size = "365,440"
+  $DataGridView.Location = "10,10"
+  $DataGridView.RowHeadersVisible = $false # don't show first column
+  $DataGridView.ColumnHeadersVisible = $true
+  $DataGridView.AutoSizeColumnsMode = "Fill"
+  $DataGridView.AllowUserToAddRows = $false
+  $DataGridView.AllowUserToResizeRows = $false
+
+  $IndicatorTypes = @("ipv4-addr","domain-name","url")
+  $IndicatorTypesColumn = New-Object System.Windows.Forms.DataGridViewComboBoxColumn
+  $IndicatorTypesColumn.Name = "Indicator Type"
+  $IndicatorTypesColumn.DataSource = $IndicatorTypes
+  $IndicatorTypesColumn.DefaultCellStyle.NullValue = $IndicatorTypes[0] 
+  $DataGridView.Columns.Add($IndicatorTypesColumn)
+
+  $IndicatorValuesColumn = New-Object System.Windows.Forms.DataGridViewTextBoxColumn
+  $IndicatorValuesColumn.Name = "Indicator Value"
+  $DataGridView.Columns.Add($IndicatorValuesColumn)
+
+  $DataGridView.RowCount = 10
   $Form.Controls.Add($DataGridView)
   $Form.ShowDialog()
 }
