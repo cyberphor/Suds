453b475a1ef6fb7bf91f750b43248d985ba3c9ad
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2023-05-05 21:00:56 -0700

    Update Suds.psm1

--- Suds.psm1
+++ Suds.psm1
@@ -8,6 +8,7 @@
 . $PSScriptRoot\Suds\Get-ProcessCreationReport.ps1
 . $PSScriptRoot\Suds\Get-SerialNumberAndCurrentUser.ps1
 . $PSScriptRoot\Suds\Get-TwoWindows.ps1
+. $PSScriptRoot\Suds\Import-Elastic.ps1
 . $PSScriptRoot\Suds\Install-RSAT.ps1
 . $PSScriptRoot\Suds\New-AdDomainAdmin.ps1
 . $PSScriptRoot\Suds\New-AdForest.ps1
