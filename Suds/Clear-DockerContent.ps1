530f74b5842b5b02df052635eedbc30271e18d2d
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2023-06-10 01:12:50 -0400

    Update Clear-DockerContent.ps1

--- Suds/Clear-DockerContent.ps1
+++ Suds/Clear-DockerContent.ps1
@@ -1,6 +1,7 @@
 function Clear-DockerContent {
     docker stop $(docker ps -aq)
     docker rm $(docker ps -aq)
-    docker system prune -af; 
+    docker volume prune -af
+    docker system prune -af
     Clear-Host
 }
